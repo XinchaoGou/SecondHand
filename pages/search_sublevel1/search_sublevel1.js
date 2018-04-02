ebd4dab31a536fc9f11fef39867a875995afc7b6
liuynkit <liuynkit@gmail.com>
2018-04-02 13:05:59 +0200

    02042018vormittag_liu

--- pages/search_sublevel1/search_sublevel1.js
+++ pages/search_sublevel1/search_sublevel1.js
@@ -6,7 +6,7 @@ Page({
    */
   data: {
 
-    array: ['按距离远近','按发布时间', '按价格从低到高', '按价格从高到低'],
+    array: ['按距离远近', '按发布时间', '按价格从低到高', '按价格从高到低'],
     objectArray: [
       {
         id: 0,
@@ -32,59 +32,67 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   },
-  
-  tosubsearch: function(){
-    wx.redirectTo({url:'../search_sublevel1/search_sublevel1'})
+
+  confirm: function(){
+    wx.switchtab({
+      url: '../search/search',
+    })
+  },
+
+  back: function(){
+    wx.switchtab({
+      url: '../search/search',
+    })
   }
 })
