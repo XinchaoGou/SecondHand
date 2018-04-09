f80f2315570b19ec08bb2db11d3516c9dbc4752a
XinchaoGou <XinchaoGou@gmail.com>
2018-04-09 16:24:38 +0200

    取消触顶刷新

--- pages/search/search.js
+++ pages/search/search.js
@@ -375,13 +375,13 @@ Page({
   },
 
   touchTOP: function (event) {
-    var that = this;
-    wx.vibrateShort();  // 使手机振动15ms  
-    wx.showNavigationBarLoading() //在标题栏中显示加载
-    this.onLoad();
-    // complete
-    wx.hideNavigationBarLoading() //完成停止加载
-    wx.stopPullDownRefresh() //停止下拉刷新
+    // var that = this;
+    // wx.vibrateShort();  // 使手机振动15ms  
+    // wx.showNavigationBarLoading() //在标题栏中显示加载
+    // this.onLoad();
+    // // complete
+    // wx.hideNavigationBarLoading() //完成停止加载
+    // wx.stopPullDownRefresh() //停止下拉刷新
   },
 
   touchBottom: function (event) {
