51a73d8e63f8b56020c473b71233d95a1d7910f1
XinchaoGou <XinchaoGou@gmail.com>
2018-06-02 21:59:29 +0200

    修改海报界面

--- pages/search/search.js
+++ pages/search/search.js
@@ -428,7 +428,7 @@ Page({
       title: '老瞄',
       desc: '找二手，上老喵！找房子，上老喵！\n找帮带，上老喵！找外卖，上老瞄！\n喵一眼，啥都有',
       path: '/pages/search/search',
-      imageUrl: '../../images/test/camera.png',
+      imageUrl: '../../images/test/poster.png',
       success: function (res) {
         // 转发成功
       },
